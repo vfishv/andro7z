6de8e77cc8a484351c5f1bd96e0ffca6aeab5329
texnician <texnician@2200ab56-e9d5-4d4c-0b78-50b4cf4d842f>
2011-08-14 13:55:14 +0000

    add android log

--- trunk/jni/7za/7za.h
+++ trunk/jni/7za/7za.h
@@ -1,6 +1,22 @@
 // Copyright (C) 2011, SNDA
+// 
 // JNI interface header
+// 
 // @author: tangyaguang@snda.com
+// 
 // @date: 2011-08-14
 
+#ifndef _7ZA_H_
+#define _7ZA_H_
+
 extern int andro7za_main(int numArgs, const char *args[]);
+
+#ifdef __cplusplus
+extern "C"{
+#endif
+
+#ifdef __cplusplus
+}
+#endif
+
+#endif  // _7ZA_H_
