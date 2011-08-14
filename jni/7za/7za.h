ce37d77a7210a0d570fa74ecaa39883a714d508f
texnician <texnician@2200ab56-e9d5-4d4c-0b78-50b4cf4d842f>
2011-08-14 17:04:06 +0000

    add run_7za and std support

--- jni/7za/7za.h
+++ jni/7za/7za.h
@@ -9,12 +9,15 @@
 #ifndef _7ZA_H_
 #define _7ZA_H_
 
-extern int andro7za_main(int numArgs, const char *args[]);
+extern int andro7za_main(int numArgs, const char *argv[]);
 
 #ifdef __cplusplus
 extern "C"{
 #endif
 
+    // Port 7zip main entry.
+    int run_7za(int argc , const char *argv[]);
+    
 #ifdef __cplusplus
 }
 #endif
