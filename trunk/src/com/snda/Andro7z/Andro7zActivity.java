e61b6c4c4ab82147c7fc7a5e3e01c7c1426a62be
texnician <texnician@2200ab56-e9d5-4d4c-0b78-50b4cf4d842f>
2011-08-14 05:18:57 +0000

    Create android project

---
+++ trunk/src/com/snda/Andro7z/Andro7zActivity.java
@@ -0,0 +1,13 @@
+package com.snda.Andro7z;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class Andro7zActivity extends Activity {
+    /** Called when the activity is first created. */
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.main);
+    }
+}
