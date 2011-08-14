4c6fb8f2b1580f9107ad00c76337f150f181a3ba
texnician <texnician@2200ab56-e9d5-4d4c-0b78-50b4cf4d842f>
2011-08-14 07:16:26 +0000

    Call 7za printUsage

--- src/com/snda/Andro7z/Andro7zActivity.java
+++ src/com/snda/Andro7z/Andro7zActivity.java
@@ -1,3 +1,10 @@
+/**
+ * Copyright (C) 2011, SNDA
+ * Main activity
+ * @author: tangyaguang@snda.com
+ * @date: 2011-08-14
+ */
+
 package com.snda.Andro7z;
 
 import android.app.Activity;
@@ -9,5 +16,7 @@ public class Andro7zActivity extends Activity {
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.main);
+        Andro7za a7z = new Andro7za();
+        a7z.printUsage();
     }
 }
